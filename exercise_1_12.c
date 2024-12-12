5111b9638898aca4d2b47844b6037a749b3b275f
Faraz Imran <farazimran21@icloud.com>
2024-12-11 22:48:25 -0500

    exercise_1_12.c

---
+++ exercise_1_12.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void) {
+  int c;
+  bool isWord = false;  // is current character part of a word?
+
+  while ((c = getchar()) != EOF) {
+    if ((c == ' ') || (c == '\n') || (c == '\t')) {
+      isWord = false;
+    } else if (!isWord) {
+      isWord = true;
+    }
+
+    if (isWord)
+      printf("%c", c);
+    else
+      printf("\n");
+  }
+
+  return 0;
+}
